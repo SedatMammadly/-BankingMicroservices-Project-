80f8c95b822f12acb507a93aebbc8151352c1c78
Sadat Mammadly <memedlisedat033@gmail.com>
2024-11-22 15:45:17 +0400

    I added UserRepository.

---
+++ security-service/src/main/java/com/sadatscode/securityservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.sadatscode.securityservice.repository;
+
+import com.sadatscode.securityservice.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
