4ee9ce8d94b272502495ea903b2d659686125077
Sadat Mammadly <memedlisedat033@gmail.com>
2024-12-11 20:09:41 +0400

    updated exceptions

--- security-service/src/main/java/com/sadatscode/securityservice/exception/ExceptionMessages.java
+++ security-service/src/main/java/com/sadatscode/securityservice/exception/ExceptionMessages.java
@@ -3,4 +3,8 @@ package com.sadatscode.securityservice.exception;
 public class ExceptionMessages {
     public static final String USER_ALREADY_EXISTS = "This user already exists.";
     public static final String USER_NOT_FOUND = "User not found.";
+    public static final String INVALID_TOKEN = "Invalid token.";
+    public static final String OLD_PASSWORD_DONT_MATCH = "Old password dont match with current password.";
+    public static final String CONFIRM_PASSWORD_DONT_MATCH = "Confirm password dont match with new password.";
+    public static final String NEW_PASSWORD_SAME = "New password same with old password.";
 }
--- security-service/src/main/java/com/sadatscode/securityservice/exception/Exceptions.java
+++ security-service/src/main/java/com/sadatscode/securityservice/exception/Exceptions.java
@@ -9,7 +9,11 @@ import org.springframework.http.HttpStatus;
 public enum Exceptions{
 
    ExistingUserException(HttpStatus.INTERNAL_SERVER_ERROR,ExceptionMessages.USER_ALREADY_EXISTS),
-   NotFoundException(HttpStatus.NOT_FOUND,ExceptionMessages.USER_NOT_FOUND);
+   NotFoundException(HttpStatus.NOT_FOUND,ExceptionMessages.USER_NOT_FOUND),
+   InvalidTokenException(HttpStatus.UNAUTHORIZED,ExceptionMessages.INVALID_TOKEN),
+   OldPassDontMatchException(HttpStatus.BAD_REQUEST,ExceptionMessages.OLD_PASSWORD_DONT_MATCH),
+   ConfirmPasswordDontMatchException(HttpStatus.BAD_REQUEST,ExceptionMessages.CONFIRM_PASSWORD_DONT_MATCH),
+   NewPasswordSameException(HttpStatus.BAD_REQUEST,ExceptionMessages.NEW_PASSWORD_SAME);
 
     private final HttpStatus httpStatus;
     private final String message;
